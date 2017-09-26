8530a0b51f9ba23762f4ae055c5616540fe0fed1
nagendra modadugu <ngm@google.com>
2017-09-26 10:55:32 -0700

    keymaster: remove test for get_hardware_features
    This method is no longer implemented on citadel.
    
    Change-Id: I78df574c2d1565f3c1324984449a2ebd3aacc506

--- src/keymaster_tests.cc
+++ src/keymaster_tests.cc
@@ -17,9 +17,6 @@ class KeymasterTest: public testing::Test {
 
   static void SetUpTestCase();
   static void TearDownTestCase();
-
- public:
-  const size_t KEY_SIZE = 16;
 };
 
 unique_ptr<nos::LinuxCitadelClient> KeymasterTest::citadelClient;
@@ -35,21 +32,6 @@ void KeymasterTest::SetUpTestCase() {
 void KeymasterTest::TearDownTestCase() {
   citadelClient->close();
   citadelClient = unique_ptr<nos::LinuxCitadelClient>();
-}
-
-TEST_F(KeymasterTest, GetHardwareFeatures) {
-  GetHardwareFeaturesRequest request;
-  GetHardwareFeaturesResponse response;
-
-  Keymaster service(*citadelClient);
-  ASSERT_NO_ERROR(service.GetHardwareFeatures(request, response));
-  EXPECT_EQ(response.is_secure(), true);
-  EXPECT_EQ(response.supports_elliptic_curve(), false);
-  EXPECT_EQ(response.supports_symmetric_cryptography(), false);
-  EXPECT_EQ(response.supports_attestation(), false);
-  EXPECT_EQ(response.supports_all_digests(), false);
-  EXPECT_EQ(response.keymaster_name(), "CitadelKeymaster");
-  EXPECT_EQ(response.keymaster_author_name(), "Google");
 }
 
 }  // namespace
