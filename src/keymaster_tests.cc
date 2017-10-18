17efd748000bd90e93372fe9d609023bb3d11ff1
nagendra modadugu <ngm@google.com>
2017-10-17 17:38:59 -0700

    keymaster: add mo' import_key tests
    Add rsa test keys and validation tests.
    
    Change-Id: I8a9702ae97a4b7916adc00450f6d0a7762fc11cb

--- src/keymaster_tests.cc
+++ src/keymaster_tests.cc
@@ -3,7 +3,9 @@
 #include "nugget/app/keymaster/keymaster.pb.h"
 #include "Keymaster.client.h"
 
+#include "src/macros.h"
 #include "src/include/keymaster/hal/3.0/types.h"
+#include "src/test-data/test-keys/rsa.h"
 
 using std::cout;
 using std::string;
@@ -12,6 +14,8 @@ using std::unique_ptr;
 using namespace android::hardware::keymaster::V3_0;
 
 using namespace nugget::app::keymaster;
+
+using namespace test_data;
 
 namespace {
 
@@ -98,7 +102,8 @@ TEST_F(KeymasterTest, ImportKeyRSAInvalidPublicExponentFails) {
 
   Keymaster service(*client);
   ASSERT_NO_ERROR(service.ImportKey(request, &response));
-  EXPECT_EQ((ErrorCode)response.error_code(), ErrorCode::INVALID_ARGUMENT);
+  EXPECT_EQ((ErrorCode)response.error_code(),
+            ErrorCode::IMPORT_PARAMETER_MISMATCH);
 }
 
 TEST_F(KeymasterTest, ImportKeyRSAKeySizeTagMisatchFails) {
@@ -119,20 +124,22 @@ TEST_F(KeymasterTest, ImportKeyRSAKeySizeTagMisatchFails) {
   param->set_integer(3);
 
   request.mutable_rsa()->set_e(3);
-  request.mutable_rsa()->set_d(std::string(64, '\0'));
+  request.mutable_rsa()->set_d(string(64, '\0'));
   request.mutable_rsa()->set_n(
-      std::string(63, '\0'));    // N does not match KEY_SIZE.
+      string(63, '\0'));    // N does not match KEY_SIZE.
 
   Keymaster service(*client);
   ASSERT_NO_ERROR(service.ImportKey(request, &response));
-  EXPECT_EQ((ErrorCode)response.error_code(), ErrorCode::INVALID_ARGUMENT);
+  EXPECT_EQ((ErrorCode)response.error_code(),
+            ErrorCode::IMPORT_PARAMETER_MISMATCH);
 
   request.mutable_rsa()->set_d(
-      std::string(63, '\0'));    // D does not match KEY_SIZE.
-  request.mutable_rsa()->set_n(std::string(64, '\0'));
+      string(63, '\0'));    // D does not match KEY_SIZE.
+  request.mutable_rsa()->set_n(string(64, '\0'));
 
   ASSERT_NO_ERROR(service.ImportKey(request, &response));
-  EXPECT_EQ((ErrorCode)response.error_code(), ErrorCode::INVALID_ARGUMENT);
+  EXPECT_EQ((ErrorCode)response.error_code(),
+            ErrorCode::IMPORT_PARAMETER_MISMATCH);
 }
 
 TEST_F(KeymasterTest, ImportKeyRSAPublicExponentTagMisatchFails) {
@@ -157,7 +164,91 @@ TEST_F(KeymasterTest, ImportKeyRSAPublicExponentTagMisatchFails) {
 
   Keymaster service(*client);
   ASSERT_NO_ERROR(service.ImportKey(request, &response));
+  EXPECT_EQ((ErrorCode)response.error_code(),
+            ErrorCode::IMPORT_PARAMETER_MISMATCH);
+}
+
+TEST_F(KeymasterTest, ImportKeyRSA1024BadKeyFails) {
+  ImportKeyRequest request;
+  ImportKeyResponse response;
+
+  KeyParameters *params = request.mutable_params();
+  KeyParameter *param = params->add_params();
+  param->set_tag((uint32_t)Tag::ALGORITHM);
+  param->set_integer((uint32_t)Algorithm::RSA);
+
+  param = params->add_params();
+  param->set_tag((uint32_t)Tag::KEY_SIZE);
+  param->set_integer(1024);
+
+  request.mutable_rsa()->set_e(65537);
+  request.mutable_rsa()->set_d(
+      string("\x01") +  /* Twiddle LSB of D. */
+      string((const char *)RSA_1024_D, sizeof(RSA_1024_D) - 1));
+  request.mutable_rsa()->set_n(RSA_1024_N, sizeof(RSA_1024_N));
+
+  Keymaster service(*client);
+  ASSERT_NO_ERROR(service.ImportKey(request, &response));
   EXPECT_EQ((ErrorCode)response.error_code(), ErrorCode::INVALID_ARGUMENT);
+
+  request.mutable_rsa()->set_e(65537);
+  request.mutable_rsa()->set_d(RSA_1024_D, sizeof(RSA_1024_D));
+  request.mutable_rsa()->set_n(
+      string("\x01") +  /* Twiddle LSB of N. */
+      string((const char *)RSA_1024_N, sizeof(RSA_1024_N) - 1));
+
+  ASSERT_NO_ERROR(service.ImportKey(request, &response));
+  EXPECT_EQ((ErrorCode)response.error_code(), ErrorCode::INVALID_ARGUMENT);
+
+  request.mutable_rsa()->set_e(3);  /* Mis-matched e. */
+  request.mutable_rsa()->set_d(RSA_1024_D, sizeof(RSA_1024_D));
+  request.mutable_rsa()->set_d(RSA_1024_N, sizeof(RSA_1024_N));
+
+  ASSERT_NO_ERROR(service.ImportKey(request, &response));
+  EXPECT_EQ((ErrorCode)response.error_code(), ErrorCode::INVALID_ARGUMENT);
+}
+
+TEST_F(KeymasterTest, ImportKeyRSASuccess) {
+  ImportKeyRequest request;
+  ImportKeyResponse response;
+
+  KeyParameters *params = request.mutable_params();
+  KeyParameter *param = params->add_params();
+  param->set_tag((uint32_t)Tag::ALGORITHM);
+  param->set_integer((uint32_t)Algorithm::RSA);
+
+  param = params->add_params();
+
+  Keymaster service(*client);
+  for (size_t i = 0; i < ARRAYSIZE(TEST_RSA_KEYS); i++) {
+    param->set_tag((uint32_t)Tag::RSA_PUBLIC_EXPONENT);
+    param->set_integer(TEST_RSA_KEYS[i].e);
+
+    request.mutable_rsa()->set_e(TEST_RSA_KEYS[i].e);
+    request.mutable_rsa()->set_d(TEST_RSA_KEYS[i].d, TEST_RSA_KEYS[i].size);
+    request.mutable_rsa()->set_n(TEST_RSA_KEYS[i].n, TEST_RSA_KEYS[i].size);
+
+    ASSERT_NO_ERROR(service.ImportKey(request, &response));
+    EXPECT_EQ((ErrorCode)response.error_code(), ErrorCode::OK);
+  }
+}
+
+TEST_F(KeymasterTest, ImportKeyRSA1024OptionalParamsAbsentSuccess) {
+  ImportKeyRequest request;
+  ImportKeyResponse response;
+
+  KeyParameters *params = request.mutable_params();
+  KeyParameter *param = params->add_params();
+  param->set_tag((uint32_t)Tag::ALGORITHM);
+  param->set_integer((uint32_t)Algorithm::RSA);
+
+  request.mutable_rsa()->set_e(65537);
+  request.mutable_rsa()->set_d(RSA_1024_D, sizeof(RSA_1024_D));
+  request.mutable_rsa()->set_n(RSA_1024_N, sizeof(RSA_1024_N));
+
+  Keymaster service(*client);
+  ASSERT_NO_ERROR(service.ImportKey(request, &response));
+  EXPECT_EQ((ErrorCode)response.error_code(), ErrorCode::OK);
 }
 
 // TODO: import bad keys (n, e, d) mismatch cases.
