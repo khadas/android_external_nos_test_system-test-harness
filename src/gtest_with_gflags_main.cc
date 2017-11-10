f49656780be9e1dc7a6fdf939a60f3a18fa3ee65
Allen Webb <allenwebb@google.com>
2017-11-10 09:50:57 -0800

    Add a list of slow tests with flags to list and disable them.
    Change-Id: I9508ab6e47d3f446a86761bd60f503431eb8c21f

--- src/gtest_with_gflags_main.cc
+++ src/gtest_with_gflags_main.cc
@@ -1,13 +1,52 @@
-#ifndef ANDROID
-#include "gflags/gflags.h"
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include <iostream>
+#include <sstream>
+
+#ifdef ANDROID
+#define FLAGS_list_slow_tests false
+#define FLAGS_disable_slow_tests false
+#else
+#include <gflags/gflags.h>
+
+DEFINE_bool(list_slow_tests, false, "List tests included in the set of slow tests.");
+DEFINE_bool(disable_slow_tests, false, "Enables a filter to disable a set of slow tests.");
 #endif  // ANDROID
-#include "gmock/gmock.h"
-#include "gtest/gtest.h"
 
 int main(int argc, char** argv) {
+  const std::vector<std::string> slow_tests{
+      "KeymasterTest.ImportKeyRSASuccess",
+      "NuggetCoreTest.HardRebootTest",};
+
   testing::InitGoogleMock(&argc, argv);
 #ifndef ANDROID
   google::ParseCommandLineFlags(&argc, &argv, true);
 #endif  // ANDROID
+
+  if (FLAGS_list_slow_tests) {
+    std::cout << "Slow tests:\n";
+    for (const auto& test : slow_tests) {
+      std::cout << "  " << test << "\n";
+    }
+    std::cout.flush();
+    exit(0);
+  }
+
+  if (FLAGS_disable_slow_tests) {
+    std::stringstream ss;
+    bool first = true;
+    for (const auto& test : slow_tests) {
+      if (first) {
+        first = false;
+      } else {
+        ss << ":";
+      }
+      ss << "-" << test;
+    }
+    ::testing::GTEST_FLAG(filter) = ss.str();
+  }
+
   return RUN_ALL_TESTS();
 }
