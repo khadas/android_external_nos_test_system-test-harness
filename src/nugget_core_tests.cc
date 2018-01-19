b652c83b670166c3b6649cd690b6790daa924df3
Bill Richardson <wfrichar@google.com>
2018-01-18 21:03:08 -0800

    Remove unused NUGGET_PARAM_REVERSE command
    This was left over from some early testing, and isn't needed now.
    
    Change-Id: I19430bacec0b0638be3f5bfc5d463d4d58274589
    Signed-off-by: Bill Richardson <wfrichar@google.com>

--- src/nugget_core_tests.cc
+++ src/nugget_core_tests.cc
@@ -52,25 +52,6 @@ TEST_F(NuggetCoreTest, GetVersionStringTest) {
   ASSERT_GT(output_buffer.size(), 0u);
 }
 
-// ./test_app --id 0 -p beef a b c d e f
-TEST_F(NuggetCoreTest, ReverseStringTest) {
-  const char test_string[] = "a b c d e f";
-  ASSERT_LT(sizeof(test_string), input_buffer.capacity());
-  input_buffer.resize(sizeof(test_string));
-  std::copy(test_string, test_string + sizeof(test_string),
-            input_buffer.begin());
-
-  ASSERT_NO_ERROR(NuggetCoreTest::client->CallApp(
-      APP_ID_NUGGET, NUGGET_PARAM_REVERSE, input_buffer, &output_buffer));
-
-  ASSERT_EQ(output_buffer.size(), sizeof(test_string));
-
-  for (size_t x = 0; x < sizeof(test_string); ++x) {
-    ASSERT_EQ(test_string[sizeof(test_string) - 1 - x],
-              output_buffer[x]) << "Failure at index: " << x;
-  }
-}
-
 TEST_F(NuggetCoreTest, SoftRebootTest) {
   ASSERT_TRUE(nugget_tools::RebootNugget(client.get(), NUGGET_REBOOT_SOFT));
 }
