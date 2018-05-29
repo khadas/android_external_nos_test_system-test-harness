021e2ca6c1ca7ad3857e2d386f54ae9f7f2e8527
Bill Richardson <wfrichar@google.com>
2018-05-29 13:28:59 -0700

    Add tests for AP UART PASSTHRU
    Change-Id: Ieaa62c37a3cd159bd3af2bbbb6c5a69643e4ecd7
    Signed-off-by: Bill Richardson <wfrichar@google.com>

--- src/nugget_core_tests.cc
+++ src/nugget_core_tests.cc
@@ -6,6 +6,7 @@
 #include <chrono>
 #include <memory>
 
+#include "avb_tools.h"
 #include "nugget_tools.h"
 #include "util.h"
 
@@ -96,6 +97,93 @@ TEST_F(NuggetCoreTest, GetLowPowerStats) {
   ASSERT_GT(stats.hard_reset_count, 0UL);
   ASSERT_GT(stats.time_since_hard_reset, 0UL);
   ASSERT_GT(stats.time_spent_awake, 0UL);
+}
+
+TEST_F(NuggetCoreTest, GetUartPassthruInBootloader) {
+  std::vector<uint8_t> send;
+  std::vector<uint8_t> get;
+
+  get.reserve(1);
+  ASSERT_NO_ERROR(NuggetCoreTest::client->CallApp(
+      APP_ID_NUGGET, NUGGET_PARAM_AP_UART_PASSTHRU,
+      send, &get), "Get UART passthru value");
+  /* Should be either on or off. If USB is active, disconnect SuzyQable */
+  ASSERT_TRUE(get[0] == NUGGET_AP_UART_OFF ||
+              get[0] == NUGGET_AP_UART_ENABLED);
+}
+
+TEST_F(NuggetCoreTest, GetUartPassthruInHLOS) {
+  std::vector<uint8_t> send;
+  std::vector<uint8_t> get;
+
+  avb_tools::BootloaderDone(client.get());
+
+  get.reserve(1);
+  ASSERT_NO_ERROR(NuggetCoreTest::client->CallApp(
+      APP_ID_NUGGET, NUGGET_PARAM_AP_UART_PASSTHRU,
+      send, &get), "Get UART passthru value");
+  /* Should be either on or off. If USB is active, disconnect SuzyQable */
+  ASSERT_TRUE(get[0] == NUGGET_AP_UART_OFF ||
+              get[0] == NUGGET_AP_UART_ENABLED);
+}
+
+
+TEST_F(NuggetCoreTest, EnableUartPassthruInBootloader) {
+  std::vector<uint8_t> send;
+  std::vector<uint8_t> get;
+
+  avb_tools::SetBootloader(client.get());
+
+  send.push_back(NUGGET_AP_UART_ENABLED);
+  get.reserve(1);
+  ASSERT_NO_ERROR(NuggetCoreTest::client->CallApp(
+      APP_ID_NUGGET, NUGGET_PARAM_AP_UART_PASSTHRU,
+      send, &get), "Enable UART passthru");
+
+  ASSERT_EQ(get[0], NUGGET_AP_UART_ENABLED);
+}
+
+TEST_F(NuggetCoreTest, DisableUartPassthruInBootloader) {
+  std::vector<uint8_t> send;
+  std::vector<uint8_t> get;
+
+  avb_tools::SetBootloader(client.get());
+
+  send.push_back(NUGGET_AP_UART_OFF);
+  get.reserve(1);
+  ASSERT_NO_ERROR(NuggetCoreTest::client->CallApp(
+      APP_ID_NUGGET, NUGGET_PARAM_AP_UART_PASSTHRU,
+      send, &get), "Disable UART passthru");
+
+  ASSERT_EQ(get[0], NUGGET_AP_UART_OFF);
+}
+
+TEST_F(NuggetCoreTest, EnableUartPassthruInHLOSFails) {
+  std::vector<uint8_t> send;
+  std::vector<uint8_t> get;
+
+  avb_tools::BootloaderDone(client.get());
+
+  send.push_back(NUGGET_AP_UART_ENABLED);
+  get.reserve(1);
+  /* This should fail */
+  ASSERT_NE(APP_SUCCESS, NuggetCoreTest::client->CallApp(
+              APP_ID_NUGGET, NUGGET_PARAM_AP_UART_PASSTHRU,
+              send, &get));
+}
+
+TEST_F(NuggetCoreTest, DisableUartPassthruInHLOSFails) {
+  std::vector<uint8_t> send;
+  std::vector<uint8_t> get;
+
+  avb_tools::BootloaderDone(client.get());
+
+  send.push_back(NUGGET_AP_UART_OFF);
+  get.reserve(1);
+  /* This should fail */
+  ASSERT_NE(APP_SUCCESS, NuggetCoreTest::client->CallApp(
+              APP_ID_NUGGET, NUGGET_PARAM_AP_UART_PASSTHRU,
+              send, &get));
 }
 
 }  // namespace
